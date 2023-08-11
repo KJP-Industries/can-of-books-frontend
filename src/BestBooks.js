bfd54a7d9109fcbf3b51db1e331935724af16166
Josh Easley <easleyjs@gmail.com>
2023-08-11 11:36:08 -0700

    added carousel and axios GET

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,7 +11,14 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    const url = `${process.env.REACT_APP_SERVER_URL}/books`
+    axios.get(url)
+    .then(({data: books}) => {
+      this.setState({books})
+    })
+    .catch(err => console.error(err))
+  };
 
   render() {
 
@@ -18,8 +28,16 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+            <Carousel>
+              {this.state.books.map((book) => ( <Carousel.Item>
+              <ExampleCarouselImage text="First slide" />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>))}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
