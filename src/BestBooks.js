f6f495854cfcc544871f17afe4d1ee1bba93d37a
Kyle Cordell <kcordell-git@outlook.com>
2023-08-12 20:55:25 -0700

    Fixes linting errors

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
-import Carousel from "react-bootstrap/Carousel";
-import Image from "react-bootstrap/Image";
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -29,23 +29,24 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length > 0 ? (
-          <Carousel wrap touch pause="hover" interval={5000}>
+          <Carousel wrap touch pause='hover' interval={5000}>
             {this.state.books.map(({ title, description, status }, idx) => (
               <Carousel.Item key={title}>
                 <Image
                   src={`https://picsum.photos/1400/300?random=${idx}`}
-                  className="w-100"
+                  className='w-100'
                   fluid
                 />
                 <Carousel.Caption>
                   <h3>{title}</h3>
-                  <p>{description}</p>
+                  <p>{description} </p>
+                  <h6>{status}</h6>
                 </Carousel.Caption>
               </Carousel.Item>
             ))}
           </Carousel>
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>{'No Books Found :('}</h3>
         )}
       </>
     );
--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
         <Navbar.Brand>Code Fellows</Navbar.Brand>
       </Navbar>
-    )
+    );
   }
 }
 
