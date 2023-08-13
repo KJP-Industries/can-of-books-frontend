5ea830b6c3bd9ca852da1dded8c453093f55862a
Kyle Cordell <kcordell-git@outlook.com>
2023-08-12 20:55:25 -0700

    Fixes linting errors

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem>
-          <Link to="/" className="nav-link text-light ms-4">
+          <Link to='/' className='nav-link text-light ms-4'>
             Home
           </Link>
         </NavItem>
         <NavItem>
-          <Link to="/about" className="nav-link text-light ms-4">
+          <Link to='/about' className='nav-link text-light ms-4'>
             About
           </Link>
         </NavItem>
