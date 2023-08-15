36b06c2bf3a4616d8635f5bf36f1d1d0c1b52d79
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-14 20:19:28 -0700

    Moves components to their own folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
-import About from './About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import BestBooks from './components/BestBooks';
+import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
