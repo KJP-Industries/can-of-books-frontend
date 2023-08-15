2a2a1275d9939fb005584dcfaaa7145969bf099b
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-14 20:50:20 -0700

    Adds the Book component

---
+++ src/components/Book.js
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+
+export default class Book extends Component {
+  render() {
+    const {
+      idx,
+      book: { title, description, status },
+    } = this.props;
+    console.log('');
+    return (
+      <>
+        <Image
+          src={`https://picsum.photos/1400/300?random=${idx}`}
+          className="w-100"
+          fluid
+        />
+        <Carousel.Caption>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <h6>{status}</h6>
+        </Carousel.Caption>
+      </>
+    );
+  }
+}
