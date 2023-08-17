58c43d28ec86191eac21af2d7ed881acad43fd9a
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-17 12:07:45 -0700

    Updates positioning of header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,21 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem>
-          <Link to='/' className='nav-link text-light ms-4'>
-            Home
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to='/about' className='nav-link text-light ms-4'>
-            About
-          </Link>
-        </NavItem>
-      </Navbar>
+      <header className="sticky-top">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>My Favorite Books</Navbar.Brand>
+          <NavItem>
+            <Link to="/" className="nav-link text-light ms-4">
+              Home
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/about" className="nav-link text-light ms-4">
+              About
+            </Link>
+          </NavItem>
+        </Navbar>
+      </header>
     );
   }
 }
