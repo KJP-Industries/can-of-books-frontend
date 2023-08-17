045ca335164b29f72d16f976eea4e827af02f00d
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-17 10:34:22 -0700

    Adds error message toast to best books

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
 import React from 'react';
+import axios from 'axios';
+
 import Carousel from 'react-bootstrap/Carousel';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import Toast from 'react-bootstrap/Toast';
 import NewButton from './NewButton';
 import BookModal from './BookModal';
 import Book from './Book';
@@ -12,23 +15,25 @@ class BestBooks extends React.Component {
       books: [],
       shouldShowModal: false,
       selectedBook: null,
+      errorMsg: null,
     };
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
     const url = `${process.env.REACT_APP_SERVER_URL}/books`;
     axios
       .get(url)
       .then(({ data: books }) => {
         this.setState({ books });
       })
-      .catch((err) => console.error(err));
-  }
+      .catch((err) => this.setErrorMsg(err.message));
+  };
 
   selectBook = (selectedBook) => {
     this.setState({
       selectedBook,
       shouldShowModal: Boolean(selectedBook),
+      errorMsg: null,
     });
   };
 
@@ -39,11 +44,15 @@ class BestBooks extends React.Component {
       .then((data) => {
         this.setState({ books: [...this.state.books, data] });
       })
-      .catch((err) => console.error(err));
+      .catch(() =>
+        this.setErrorMsg(
+          'There was an error adding your book. Please try again.'
+        )
+      );
   };
 
   deleteBook = (deleteBook) => {
-    const deleteUrl = `${process.env.REACT_APP_SERVER_URL}/books/${deleteBook._id}`;
+    const deleteUrl = `${process.env.REACT_APP_SERVER_URL}/books/${deleteBook.id}`;
     axios
       .delete(deleteUrl, deleteBook)
       .then(() => {
@@ -51,7 +60,11 @@ class BestBooks extends React.Component {
         const books = oldBooks.filter((book) => book._id !== deleteBook._id);
         this.setState({ books });
       })
-      .catch((err) => console.error(err));
+      .catch(() =>
+        this.setErrorMsg(
+          'An error occurred deleting this book. Please try again.'
+        )
+      );
   };
 
   toggleModal = () => {
@@ -59,10 +72,17 @@ class BestBooks extends React.Component {
     this.setState({
       shouldShowModal: !shouldShowModal,
       selectedBook: this.state.selectedBook ? null : this.state.selectedBook,
+      errorMsg: null,
     });
   };
 
-  render() {
+  setErrorMsg = (errorMsg = null) => {
+    this.setState({ errorMsg });
+  };
+
+  render = () => {
+    const TOAST_TIMEOUT = 10000;
+
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
@@ -87,11 +107,30 @@ class BestBooks extends React.Component {
           selectedBook={this.state.selectedBook}
           addBook={this.addBook}
           deleteBook={this.deleteBook}
+          errorMsg={this.state.errorMsg}
+          setErrorMsg={this.setErrorMsg}
         />
         <NewButton toggleModal={this.toggleModal} btnText={'Add Book'} />
+        <ToastContainer
+          className="p-3"
+          position="bottom-center"
+          style={{ zIndex: 1 }}
+        >
+          <Toast
+            bg="danger"
+            show={this.state.errorMsg}
+            delay={TOAST_TIMEOUT}
+            autohide
+            onClose={() => this.setErrorMsg()}
+          >
+            <Toast.Body className="text-white">
+              {this.state.errorMsg}
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
       </>
     );
-  }
+  };
 }
 
 export default BestBooks;
