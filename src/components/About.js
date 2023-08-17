798fd9bdd41497607167840033a748e97257449e
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-17 12:09:55 -0700

    Updates tags and styling of About and BestBooks components

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,15 @@ import { Component } from 'react';
 class Profile extends Component {
   render() {
     return (
-      <>
+      <main className="h-100">
         <p>KJP industries developers: Josh, Kyle and Peter </p>
         <p>Check out our projects at:</p>
         <p>
-          <a href='https://github.com/orgs/KJP-Industries/repositories'>
+          <a href="https://github.com/orgs/KJP-Industries/repositories">
             {'https://github.com/orgs/KJP-Industries/repositories'}
           </a>
         </p>
-      </>
+      </main>
     );
   }
 }
