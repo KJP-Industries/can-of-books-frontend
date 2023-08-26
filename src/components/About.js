9e429793cedc06fe79c3cdee9e4e7b81e64a81a9
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-26 15:57:06 -0700

    Updates styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,11 @@ import { Component } from 'react';
 class Profile extends Component {
   render() {
     return (
-      <main className='h-100'>
+      <main className="h-100 p-5">
         <p>KJP industries developers: Josh, Kyle and Peter </p>
         <p>Check out our projects at:</p>
         <p>
-          <a href='https://github.com/orgs/KJP-Industries/repositories'>
+          <a href="https://github.com/orgs/KJP-Industries/repositories">
             {'https://github.com/orgs/KJP-Industries/repositories'}
           </a>
         </p>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Profile = () => {
@@ -10,11 +13,15 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
+      <Container className="py-5">
+        <Col xs={12} md={6} lg={2}>
+          <Card>
+            <Card.Img fluid src={user.picture} alt={user.name} />
+            <Card.Title className="p-2">{user.name}</Card.Title>
+            <Card.Body>{user.email}</Card.Body>
+          </Card>
+        </Col>
+      </Container>
     )
   );
 };
