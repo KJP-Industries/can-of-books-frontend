d5a30c276096fdb3aee10bdcde9084bca1dac8ef
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-26 13:46:54 -0700

    Adds Auth0Provider context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENTID}
+      authorizationParams={{
+        redirect_uri: process.env.REACT_APP_REDIRECT,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
