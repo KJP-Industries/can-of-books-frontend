55fe24738628bf38ea6a03b40cc06577a1a5a48d
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-26 17:33:40 -0700

    Adds user image

--- src/components/AuthButtons.js
+++ src/components/AuthButtons.js
@@ -2,10 +2,33 @@ import React from 'react';
 
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
+import Image from 'react-bootstrap/Image';
 
-export default class AuthButtons extends React.Component {
-  render() {
-    const { isAuthenticated } = this.props;
-    return <>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</>;
+import { withAuth0 } from '@auth0/auth0-react';
+
+export default withAuth0(
+  class AuthButtons extends React.Component {
+    render() {
+      const { isAuthenticated, auth0 } = this.props;
+      return (
+        <>
+          {isAuthenticated ? (
+            <>
+              <Image
+                src={auth0.user.picture}
+                roundedCircle
+                fluid
+                height="40"
+                width="40"
+                className="me-3 mh-1"
+              />
+              <LogoutButton />
+            </>
+          ) : (
+            <LoginButton />
+          )}
+        </>
+      );
+    }
   }
-}
+);
